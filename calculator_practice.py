116671f15511145c61f3679bfa43a942ef86203a
Abby Gessesse <abbygessesse@MacBook-Air-329.local>
2025-09-23 21:27:12 -0700

    calculator practice

--- calculator_practice.py
+++ calculator_practice.py
@@ -15,5 +15,12 @@ def run_total_seconds(easy_miles, tempo_miles):
     # A runner runs easy_miles at an easy pace (8:15 per mile)
     # and tempo_miles miles at tempo pace (7:12 per mile). 
     # TODO: Write a function to calculate the total time of the run in seconds.
-    return 0
 
+    easy_seconds = easy_miles * 495
+    # easy: 8*60 + 15 = 495 seconds = 1 easy mile
+    tempo_seconds = tempo_miles * 432
+    # tempo: 7*60 + 12 = 432 seconds = 1 tempo mile
+    totalTime = easy_seconds + tempo_seconds
+    return totalTime
+
+print(run_total_seconds(1,1))
